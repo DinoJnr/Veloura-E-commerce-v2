2fce3ab0cd38a447ef06a201a553d6b54f277cf2
dino <philipadebiyi2@gmail.com>
2026-05-11 01:47:26 +0100

    Fix case sensitivity for authMiddleware

--- Backend/VelouraBackend/routes/Admin.route.js
+++ Backend/VelouraBackend/routes/Admin.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router  = express.Router();
 const upload  = require("../middleware/upload.js");
-const { protect, adminOnly } = require("../middleware/authMiddleware");
+const { protect, adminOnly } = require("../middleware/authmiddleware");
 
 const {
   registerUser,
